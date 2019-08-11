9a375c3665b1da4532f0edccf6a6e944d8c3adf3
Rahul Lal <Rahul21Lal@gmail.com>
2019-08-11 17:37:59 +1200

    Pic saved locally

--- client/components/Doctor.jsx
+++ client/components/Doctor.jsx
@@ -1,13 +1,26 @@
 import React from 'react'
 
 const Doctor = props => {
-    <div className='column'>
-        <h2>{props.name}</h2>
-        <h3>{props.actor}</h3>
-        <p>
-            {props.startedYear} - {props.finishedYear}
-        </p>
-        <img src={props.image} width='450' height='575' />
+    // <div className='column'>
+    //     <h2>{props.name}</h2>
+    //     <h3>{props.actor}</h3>
+    //     <p>
+    //         {props.startedYear} - {props.finishedYear}
+    //     </p>
+    //     <img src={props.image} width='450' height='575' />
+    // </div>
+
+    <div className='doctor-wrapper'>
+        <div className='doctor'>
+            <div className='doctor-name-plate'>
+                <span className='doctor-name'>{props.name}</span>
+                <span className='doctor-breed'>{props.actor}</span>
+                <br />
+                <img src={`/images/${props.image}`} className='doctor-pic' />
+            </div>
+            <span className='doctor-superpower'>{props.startedYear}</span>
+            <span className='doctor-superpower'>{props.finishedYear}</span>
+        </div>
     </div>
 }
 
